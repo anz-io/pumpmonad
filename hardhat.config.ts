5338ddd4d21ce6adf303059a48641de8302a8dda
wyf-ACCEPT <43724111+wyf-ACCEPT@users.noreply.github.com>
2025-08-01 21:43:52 +0800

    [feat] add deploy scripts and deploy on sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,19 @@ import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.RPC_SEPOLIA,
+      accounts: [
+        process.env.PRIVATE_KEY_ADMIN!,
+      ]
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.API_ETHERSCAN!,
+    }
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,19 @@
-import { UpgradeableUSDC } from "../typechain-types"
 import "dotenv/config"
 import { deployContract, deployUpgradeableContract } from "./utils"
+import { ERC20, PumpMonadStaking, PumpToken } from "../typechain-types"
 
 async function main() {
-  await deployUpgradeableContract(
-    "UpgradeableUSDC", []
-  )
+  const wmonad = await deployContract("MockMonad", [], true) as ERC20
+  const pumpmonad = await deployContract("PumpToken", [], true) as PumpToken
+  const pumpmonadstaking = await deployUpgradeableContract(
+    "PumpMonadStaking", [await wmonad.getAddress(), await pumpmonad.getAddress()], true
+  ) as PumpMonadStaking
+  
+  await pumpmonadstaking.setAllowNormalUnstake(true)
+  await pumpmonadstaking.setAllowInstantUnstake(true)
+  await pumpmonadstaking.setAllowClaim(true)
+  
+  await pumpmonad.setMinter(await pumpmonadstaking.getAddress(), true)
 }
 
 main()
