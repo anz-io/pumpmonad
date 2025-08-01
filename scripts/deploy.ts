f54bdd07f8588c633ebcfdce0a37bf16090becd5
wyf-ACCEPT <43724111+wyf-ACCEPT@users.noreply.github.com>
2025-08-01 21:59:11 +0800

    [feat] update script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,29 @@
 import "dotenv/config"
 import { deployContract, deployUpgradeableContract } from "./utils"
-import { ERC20, PumpMonadStaking, PumpToken } from "../typechain-types"
+import { ERC20, MockMonad, PumpMonadStaking, PumpToken } from "../typechain-types"
+import { parseEther } from "ethers"
+import { ethers } from "hardhat"
 
 async function main() {
-  const wmonad = await deployContract("MockMonad", [], true) as ERC20
+  const [owner] = await ethers.getSigners()
+
+  const wmonad = await deployContract("MockMonad", [], true) as MockMonad
   const pumpmonad = await deployContract("PumpToken", [], true) as PumpToken
   const pumpmonadstaking = await deployUpgradeableContract(
-    "PumpMonadStaking", [await wmonad.getAddress(), await pumpmonad.getAddress()], true
+    "PumpMonadStaking", [await pumpmonad.getAddress(), await wmonad.getAddress()], true
   ) as PumpMonadStaking
   
   await pumpmonadstaking.setAllowNormalUnstake(true)
   await pumpmonadstaking.setAllowInstantUnstake(true)
   await pumpmonadstaking.setAllowClaim(true)
+  await pumpmonadstaking.setStakeAssetCap(await parseEther("10000"))
   
   await pumpmonad.setMinter(await pumpmonadstaking.getAddress(), true)
+
+  await wmonad.mint(owner.address, parseEther("10000"))
+  await wmonad.approve(await pumpmonadstaking.getAddress(), parseEther("10000"))
+
+  await pumpmonadstaking.stake(parseEther("100"))
 }
 
 main()
